ca7e6af16e9b09dc81f797879f07a36d662b96f4
groovecoder <luke.crouch@gmail.com>
2016-02-03 13:26:26 -0600

    for #4: move validate_domain_authorizations to tasks module

--- domains/management/commands/validate_domain_authorizations.py
+++ domains/management/commands/validate_domain_authorizations.py
@@ -1,32 +1,10 @@
-from datetime import datetime
-
-from decouple import config
-from dnslib.dns import DNSRecord, DNSQuestion, QTYPE
-
 from django.core.management.base import BaseCommand
 
-from ...models import DomainAuthorization
-
-
-# Default: OpenDNS
-DNS_VALIDATION_SERVER = config('DNS_VALIDATION_SERVER', '208.67.222.222')
+from ...tasks import validate_domain_authorizations
 
 
 class Command(BaseCommand):
     help = 'Validate all pending domain authorizations.'
 
     def handle(self, *args, **kwargs):
-        domain_authorizations = DomainAuthorization.objects.filter(
-            status='pending'
-        )
-        for auth in domain_authorizations:
-            if auth.type == 'dns':
-                txt_record = auth.token + '.' + auth.domain
-                q = DNSRecord(q=DNSQuestion(txt_record, getattr(QTYPE, 'TXT')))
-                record_pkt = q.send(DNS_VALIDATION_SERVER, 53)
-                record = DNSRecord.parse(record_pkt)
-                if str(record.a.rdata).strip('"') == auth.token:
-                    auth.status = 'valid'
-                    auth.validated = datetime.now()
-                    # TODO: determine a good value for auth.expires
-                    auth.save()
+        validate_domain_authorizations()
