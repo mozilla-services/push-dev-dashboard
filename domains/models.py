d36cf403ddecf0ce165d42a0de001e22cc0a6efc
groovecoder <luke.crouch@gmail.com>
2016-02-04 11:36:38 -0600

    move validation logic to model + manager

--- domains/models.py
+++ domains/models.py
@@ -1,9 +1,19 @@
 from __future__ import unicode_literals
+from datetime import datetime, timedelta
 import uuid
+
+from decouple import config
+from dnslib.dns import DNSRecord, DNSQuestion, QTYPE
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
+from .managers import DomainAuthorizationManager
+
+
+# Default: OpenDNS
+DNS_VALIDATION_SERVER = config('DNS_VALIDATION_SERVER', '208.67.222.222')
 
 AUTHORIZATION_STATUS_CHOICES = (
     ('unknown', 'unknown'),
@@ -36,9 +46,33 @@ class DomainAuthorization(models.Model):
     validated = models.DateTimeField(blank=True, null=True)
     expires = models.DateTimeField(blank=True, null=True)
 
+    objects = DomainAuthorizationManager()
+
     def __unicode__(self):
         return "%s: %s" % (self.user.username, self.domain)
 
     def save(self, *args, **kwargs):
         self.token = uuid.uuid5(uuid.NAMESPACE_DNS, str(self.domain))
         super(DomainAuthorization, self).save(*args, **kwargs)
+
+    def get_dns_txt_record(self):
+        txt_record = self.token + '.' + self.domain
+        q = DNSRecord(q=DNSQuestion(txt_record, getattr(QTYPE, 'TXT')))
+        record_pkt = q.send(DNS_VALIDATION_SERVER, 53)
+        record = DNSRecord.parse(record_pkt)
+        return record
+
+    def validate(self):
+        if self.type == 'dns':
+            record = self.get_dns_txt_record()
+            if str(record.a.rdata).strip('"') == self.token:
+                self.status = 'valid'
+                self.validated = timezone.make_aware(
+                    datetime.now(),
+                    timezone.get_current_timezone()
+                )
+                self.expires = timezone.make_aware(
+                    datetime.now() + timedelta(1),
+                    timezone.get_current_timezone()
+                )
+                self.save()
--- domains/tests.py
+++ domains/tests.py
@@ -1,6 +1,9 @@
+from datetime import datetime, timedelta
+
 from uuid import UUID
 
 from django.test import TestCase
+from django.utils import timezone
 
 from model_mommy import mommy
 
@@ -22,3 +25,37 @@ class DomainAuthorizationTests(TestCase):
         da.save()
         self.assertIsInstance(da.token, UUID)
         self.assertEqual(5, da.token.version)
+
+
+class DomainAuthorizationManagerTests(TestCase):
+    def setUp(self):
+        self.tz_aware_now = timezone.make_aware(
+            datetime.now(), timezone.get_current_timezone()
+        )
+        # A non-expired, valid domain auth
+        mommy.make(DomainAuthorization,
+                   expires=self.tz_aware_now + timedelta(1),
+                   status='valid')
+        # An expired, valid domain auth
+        mommy.make(DomainAuthorization,
+                   expires=self.tz_aware_now - timedelta(1),
+                   status='valid')
+        # A pending domain auth
+        mommy.make(DomainAuthorization,
+                   expires=self.tz_aware_now + timedelta(1),
+                   status='pending')
+
+    def test_expired(self):
+        expired_domain_auths = DomainAuthorization.objects.expired()
+        self.assertEquals(1, len(expired_domain_auths))
+        for domain_auth in expired_domain_auths:
+            self.assertTrue(domain_auth.expires <= self.tz_aware_now)
+
+    def test_need_validation(self):
+        domain_auths = DomainAuthorization.objects.need_validation()
+        self.assertEquals(2, len(domain_auths))
+        for domain_auth in domain_auths:
+            self.assertTrue(
+                (domain_auth.expires <= self.tz_aware_now) or
+                (domain_auth.status == 'pending')
+            )
