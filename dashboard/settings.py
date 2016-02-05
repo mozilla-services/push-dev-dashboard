3d789b1ef30d2ecbc04bdda991dd40a2b9dad461
groovecoder <luke.crouch@gmail.com>
2016-02-05 11:10:14 -0600

    add swagger for api docs

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'rest_framework',
+    'rest_framework_swagger',
 
     'dashboard.socialaccount.providers.fxa',
     'landing',
