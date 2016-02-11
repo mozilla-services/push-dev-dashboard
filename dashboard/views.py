b2144efa588e73c97a8e2d5c488ada89d645ae43
groovecoder <luke.crouch@gmail.com>
2016-02-11 16:52:27 -0600

    flake8 fix

--- dashboard/views.py
+++ dashboard/views.py
@@ -12,7 +12,9 @@ class Home(TemplateView):
         domains = None
         push_apps = None
         if (self.request.user.is_authenticated()):
-            domains = DomainAuthorization.objects.filter(user=self.request.user)
+            domains = DomainAuthorization.objects.filter(
+                user=self.request.user
+            )
             push_apps = PushApplication.objects.filter(user=self.request.user)
         context['domains'] = domains
         context['push_apps'] = push_apps
