3ad4de737380d0842f40c82301d1f748c1b20f2b
groovecoder <luke.crouch@gmail.com>
2016-04-26 12:48:29 -0500

    fix #182: add copy button for vapid token

--- push/static/js/validation.js
+++ push/static/js/validation.js
@@ -1,10 +1,62 @@
 (function(win, doc, $) {
     'use strict';
 
-    // When the page loads, select the vapid key token, so the user can easily
-    // copy it.
-    // TODO: https://github.com/mozilla-services/push-dev-dashboard/issues/182
-    // for code that could potentially add a "copy-to-clipboard" button
-    $('#vapid-key-token').select();
+    function createNode(text) {
+        var node = document.createElement('pre');
+        node.style.width = '1px';
+        node.style.height = '1px';
+        node.style.position = 'fixed';
+        node.style.top = '5px';
+        node.textContent = text;
+        return node;
+    }
 
+    function copyNode(node) {
+        var selection = getSelection();
+        selection.removeAllRanges();
+
+        var range = document.createRange();
+        range.selectNodeContents(node);
+        selection.addRange(range);
+
+        document.execCommand('copy');
+        selection.removeAllRanges();
+    }
+
+    function copyText(text) {
+        var node = createNode(text);
+        document.body.appendChild(node);
+        copyNode(node);
+        document.body.removeChild(node);
+    }
+
+    function copyInput(node) {
+        node.select();
+        document.execCommand('copy');
+        getSelection().removeAllRanges();
+    }
+
+    function isFormInput(element) {
+        return element.nodeName === 'INPUT' || element.nodeName === 'TEXTAREA';
+    }
+
+    $(document).on('click', '.js-copy-button', function() {
+        var text;
+        if ((text = this.getAttribute('data-clipboard-text')) !== null) {
+            copyText(text);
+        } else {
+            var container = this.closest('.js-copy-container');
+            var content = container.querySelector('.js-copy-target');
+            if (isFormInput(content)) {
+                if (content.type === 'hidden') {
+                    copyText(content.value);
+                } else {
+                    copyInput(content);
+                }
+            } else {
+                copyNode(content);
+            }
+        }
+        this.blur();
+    });
 })(window, document, jQuery);
