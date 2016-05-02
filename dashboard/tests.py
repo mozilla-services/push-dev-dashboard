72e1453ff443ffc48943edbf2de616e3769dad9a
groovecoder <luke.crouch@gmail.com>
2016-05-02 10:41:53 -0500

    for #200: start dashboard selenium tests

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,7 +1,18 @@
+from unittest import skipIf
+from decouple import config
+
 from model_mommy import mommy
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 from django.contrib.auth.models import User
+from django.contrib.staticfiles.testing import LiveServerTestCase
 from django.test import TestCase, override_settings
+
+
+WEBDRIVER_TIMEOUT = config('TESTING_WEBDRIVER_TIMEOUT', 0.0, cast=float)
 
 
 # PipelineCachedStorage breaks in some test environments (like Travis)
@@ -45,3 +56,48 @@ class DashboardUrlTestsFor200(UrlTestsFor200):
         self.signed_in_urls = (
             '/en/',
         )
+
+
+@skipIf(WEBDRIVER_TIMEOUT == 0, "Skipping SeleniumTests.")
+class SeleniumTests(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(SeleniumTests, cls).setUpClass()
+        cls.selenium = WebDriver()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(SeleniumTests, cls).tearDownClass()
+
+    def setUp(self):
+        self.site = config('TESTING_SITE', None)
+        self.email = config('TESTING_FXA_ACCOUNT_EMAIL', None)
+        self.password = config('TESTING_FXA_ACCOUNT_PASSWORD', None)
+
+    def _wait_for_element(self, css_selector):
+        wait = WebDriverWait(self.selenium, WEBDRIVER_TIMEOUT)
+        wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, css_selector))
+        )
+
+    def _wait_for_element_and_click(self, css_selector):
+        self._wait_for_element(css_selector)
+        self.selenium.find_element_by_css_selector(css_selector).click()
+
+    def test_login(self):
+        self.selenium.get('%s' % self.site)
+        self.selenium.find_element_by_css_selector("a.sign-in-btn").click()
+
+        # On FxA site, click "Have an account? Sign in." and fill in the form
+        self._wait_for_element_and_click('a.sign-in')
+        self.selenium.find_element_by_css_selector('input.email').send_keys(
+            self.email
+        )
+        self.selenium.find_element_by_css_selector('input.password').send_keys(
+            self.password
+        )
+        self.selenium.find_element_by_css_selector("button#submit-btn").click()
+
+        # Back at dashboard; should see manage apps button
+        self._wait_for_element('a#manage-push-apps-btn')
